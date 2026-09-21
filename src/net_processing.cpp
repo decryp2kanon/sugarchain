75906fd6f7a1bb48ce1330cdf2c95653d1f1bb7a
decryp2kanon <60179867+decryp2kanon@users.noreply.github.com>
2026-09-21 22:42:18 +0900

    Distribute IBD block requests across peers

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -3664,8 +3664,10 @@ bool PeerLogicValidation::SendMessages(CNode* pto, std::atomic<bool>& interruptM
             // the first peer processed by SendMessages() cannot normally claim
             // the entire currently available range before other peers are
             // scheduled.
+            static const unsigned int BLOCK_DOWNLOAD_BATCH_DIVISOR = 256;
             const unsigned int BLOCK_DOWNLOAD_BATCH_LIMIT =
-                std::max(1024u, static_cast<unsigned int>(MAX_BLOCKS_IN_TRANSIT_PER_PEER / 16));
+                std::max(1u, static_cast<unsigned int>(
+                    MAX_BLOCKS_IN_TRANSIT_PER_PEER / BLOCK_DOWNLOAD_BATCH_DIVISOR));
 
             const unsigned int nBlocksToRequest =
                 std::min(static_cast<unsigned int>(MAX_BLOCKS_IN_TRANSIT_PER_PEER - state.nBlocksInFlight),
