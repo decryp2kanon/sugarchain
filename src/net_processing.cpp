22639a4a9ca235f38cfab9f7fe29beeecff6b48b
decryp2kanon <60179867+decryp2kanon@users.noreply.github.com>
2026-09-22 10:16:06 +0900

    Stop redundant header sync pipelines

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -1401,7 +1401,7 @@ bool static ProcessHeadersMessage(CNode *pfrom, CConnman *connman, const std::ve
             nodestate->m_last_block_announcement = GetTime();
         }
 
-        if (nCount == MAX_HEADERS_RESULTS) {
+        if (nCount == MAX_HEADERS_RESULTS && received_new_header) {
             // Headers message had its maximum size; the peer may have more headers.
             // TODO: optimize: if pindexLast is an ancestor of chainActive.Tip or pindexBestHeader, continue
             // from there instead.
