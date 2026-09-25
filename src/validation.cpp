f69af7ff9620c96bb816c73b112ab7ee7a97dcfb
decryp2kanon <60179867+decryp2kanon@users.noreply.github.com>
2026-09-26 02:17:19 +0900

    validation: suppress per-block tip logging during IBD

--- src/validation.cpp
+++ src/validation.cpp
@@ -2276,6 +2276,7 @@ void static UpdateTip(const CBlockIndex *pindexNew, const CChainParams& chainPar
             DoWarning(strWarning);
         }
     }
+    if (IsInitialBlockDownload()) return;
     LogPrintf("%s: new best=%s height=%d version=0x%08x log2_work=%.8g tx=%lu date='%s' progress=%f cache=%.1fMiB(%utxo)", __func__,
       pindexNew->GetBlockHash().ToString(), pindexNew->nHeight, pindexNew->nVersion,
       log(pindexNew->nChainWork.getdouble())/log(2.0), (unsigned long)pindexNew->nChainTx,
